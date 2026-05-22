5c5804cf855db4e402ad51b9a08f37e7efa3a827
George Moroz <agricolamz@gmail.com>
2026-05-22 20:41:32 +0300

    update

--- assistant_brain.R
+++ assistant_brain.R
@@ -1,6 +1,5 @@
 suppressPackageStartupMessages(library(tidyverse))
 library(logger)
-library(gmailr)
 
 path_to_logs <- "~/work/my_R_assistant/logs/assistant_logs.txt"
 path_to_scripts <- "~/work/my_R_assistant/scripts/"
@@ -40,53 +39,14 @@ log_info("Количество задач в файле: {n_tasks}")
 seq_along(tasks$id) |> 
   walk(function(task_id){
     current_task <- tasks$task[task_id]
-    log_info("{current_task} начинается, запускаю {tasks$skill[task_id]}")
+    logger::log_info("🤖 {current_task} начинается, запускаю {tasks$skill[task_id]}")
     
     if(exists(tasks$skill[task_id])){
       logger::log_debug("🤖  Умение {tasks$skill[task_id]} есть.")  
     } else {
       logger::log_error("🤖  Нет умения {tasks$skill[task_id]}.")
-      stop()
-    }
-    
-    
-    if(file.access(script) == 0){
-      log_debug("✔️   Скрипт существует, запускаю")
+      logger::log_info("🤖  Меняю статус задачи {tasks$skill[task_id]} на ignore.")
       
-      if(tools::file_ext(script) == "R"){
-        
-        if(!is.na(tasks$params[task_id])){
-          tasks$params[task_id] |>
-            jsonlite::fromJSON() ->
-            params
-
-          seq_along(params) |>
-            walk(function(i){
-              assign(names(params)[[i]],
-                     params[[i]],
-                     envir = parent.frame(n = 4))
-            })
-        }
-        
-        source(script, local = TRUE)  
-      } else {
-        log_error("🫣  Пока поддерживаются только R скрипты")
-      }
-      
-      if(tasks$after_finished[task_id] == "remove"){
-        log_info("🗑  Удаляю задачу {current_task} из списка.")
-
-        path_to_tasks |> 
-          read_csv(show_col_types = FALSE,
-                   progress = FALSE) |> 
-          filter(id != task_id) |> 
-          write_csv(file = path_to_tasks, na = "")
-      }
-      
-    } else {
-      log_error("⚠️   Скрипт {tasks$skill[task_id]} не найден")
-      log_info("🙈️   Меняю статус задачи {tasks$skill[task_id]} на ignore.")
-
       path_to_tasks |> 
         read_csv(show_col_types = FALSE,
                  progress = FALSE) |> 
@@ -94,17 +54,33 @@ seq_along(tasks$id) |>
         write_csv(file = path_to_tasks, na = "")
       
       if(curl::has_internet()){
-        
         sent_gmail_message(subject = "Нет скрипта для задачи",
                            message = str_glue("Я не нашел скрипт {tasks$skill[task_id]} для задачи {tasks$task[task_id]} и поменял ее статус на `ignore`."),
                            log_message = "Отправляю письмо на gmail с сообщением об ошибке")
-        
       } else {
-        log_error("🦦  Интернета нет, так что я не сообщил о проблеме")
-        log_info("🦦  Добавляю отправку письма с сообщением о проблеме в список задач")
+        logger::log_error("🦦  Интернета нет, так что я не сообщил о проблеме")
+        logger::log_info("🦦  Добавляю отправку письма с сообщением о проблеме в список задач")
       }
     }
-    log_info("{current_task} закончена")
+    
+    tasks$params[task_id] |> 
+      yaml::read_yaml(text = _) ->
+      params
+    
+    do.call(what = tasks$skill[task_id],
+            args = params)
+    
+    log_info("🤖  Задача {current_task} завершена.")
+    
+    if(tasks$after_finished[task_id] == "remove"){
+      logger::log_info("🗑  Удаляю задачу {current_task} из списка.")
+      
+      path_to_tasks |> 
+        read_csv(show_col_types = FALSE,
+                 progress = FALSE) |> 
+        filter(id != task_id) |> 
+        write_csv(file = path_to_tasks, na = "")
+    }
   })
 
 log_info("🧮  Задачи выполнены, переиндексирую список задач")
--- skills/ollama_call/scripts/ollama_call.R
+++ skills/ollama_call/scripts/ollama_call.R
@@ -33,30 +33,30 @@ ollama_call <- function(ollama_model = "gemma4b26",
   }
 
   if(exists("ollama_model")){
-    log_debug("🦙  параметр `ollama_model` есть")  
+    logger::log_debug("🦙  параметр `ollama_model` есть")  
   } else {
-    log_error("🦙  нет параметра `ollama_model`")
+    logger::log_error("🦙  нет параметра `ollama_model`")
     stop()
   }
   
   if(exists("ollama_message")){
-    log_debug("🦙  параметр `ollama_message` есть")  
+    logger::log_debug("🦙  параметр `ollama_message` есть")  
   } else {
-    log_error("🦙  нет параметра `ollama_message`")
+    logger::log_error("🦙  нет параметра `ollama_message`")
     stop()
   }
   
   if(ollamar::test_connection(logical = TRUE)){
-    log_debug("🦙  Ollama запущена")  
+    logger::log_debug("🦙  Ollama запущена")  
   } else {
-    log_error("🦙  Ollama не запущена")
+    logger::log_error("🦙  Ollama не запущена")
     stop()
   }
   
   if(ollamar::model_avail(ollama_model)){
-    log_debug("🦙  Модель {ollama_model} есть в списке доступных моделей")  
+    logger::log_debug("🦙  Модель {ollama_model} есть в списке доступных моделей")  
   } else {
-    log_error("🦙  Модели {ollama_model} нет в списке доступных моделей")
+    logger::log_error("🦙  Модели {ollama_model} нет в списке доступных моделей")
     stop()
   }
   
@@ -73,7 +73,7 @@ ollama_call <- function(ollama_model = "gemma4b26",
     sent_gmail_message(subject = "Ответ модели Ollama",
                        message = str_glue("Вот ответ модели:\n\n{result}"))
   } else {
-    log_warning("🦦  Нет интернет соединения, так что я не отправил ответа модели")
-    log_info("🦦   Добавляю отправку письма с ответом модели в список задач")
+    logger::log_warning("🦦  Нет интернет соединения, так что я не отправил ответа модели")
+    logger::log_info("🦦   Добавляю отправку письма с ответом модели в список задач")
   }
 }
